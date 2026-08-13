7275807351f687a9e8a882da45ddea1cec1bb08d
Gabriel Perrott <gabrielperrott@gmail.com>
2026-08-13 18:27:48 +1200

    add model for quotes call to external API

--- models/lotr.ts
+++ models/lotr.ts
@@ -4,3 +4,22 @@ export interface quoteInfo{
 }
 
 
+
+//// Interfaces for External API
+
+export interface Quotes {
+    docs:   Doc[];
+    total:  number;
+    limit:  number;
+    offset: number;
+    page:   number;
+    pages:  number;
+}
+
+export interface Doc {
+    _id:       string;
+    dialog:    string;
+    movie:     string;
+    character: string;
+    id:        string;
+}
