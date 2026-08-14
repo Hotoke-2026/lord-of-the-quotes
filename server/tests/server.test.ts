fa953c5f0d73f750555c72f14c5279a70b93e97d
Gabriel Perrott <gabrielperrott@gmail.com>
2026-08-14 12:34:34 +1200

    importing nock and start structuring mock requests

--- server/tests/server.test.ts
+++ server/tests/server.test.ts
@@ -1,6 +1,15 @@
 import {describe, it, expect} from 'vitest';
 import supertest from 'supertest';
 import server from '../server';
+import nock from 'nock';
+
+const scope = nock('https://the-one-api.dev').get('/v2/quote').reply(200, {
+    "_id": "5cd96e05de30eff6ebcce961",
+    "dialog": "What were you thinking Peregrin Took? What service can a Hobbit offer such a great lord of men?",
+    "movie": "5cd95395de30eff6ebccde5d",
+    "character": "5cd99d4bde30eff6ebccfe2e",
+    "id": "5cd96e05de30eff6ebcce961"
+})
 
 describe('GET /api/v1/quoteinfo', () => {
   it('should return a 200 status code', async () => {
